ab0a134a9a5c4eaf6854ab8aafed5ed56cd9f27d
KeepKindling <flashywizard127@gmail.com>
2022-07-31 11:02:27 +0000

    add js questions and answers array

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+let questions = [{
+    question: 'How old was Ashe when he began his journey?',
+    choice_1: '12',
+    choice_2: '10',
+    choice_3: '11'
+},
+{
+    question: 'What is Mistys favorite Pokemon?',
+    choice_1: 'Seel',
+    choice_2: 'Starmie',
+    choice_3: 'Staryu'
+},
+{
+    question: 'How many siblings does Brock have?',
+    choice_1: '9',
+    choice_2: '5',
+    choice_3: '3'
+},
+{
+    question: "Who is Ashe's rival?",
+    choice_1: 'Gary',
+    choice_2: 'Brock',
+    choice_3: 'Misty'
+},
+{
+    question: 'What was the first Pokemon that Ashe caught?',
+    choice_1: 'Pidgey',
+    choice_2: 'Mankey',
+    choice_3: 'Caterpie'
+},
+{
+    question: 'Which giant Pokemon attacked the city Porta Vista?',
+    choice_1: 'Kangaskhan',
+    choice_2: 'Tentacruel',
+    choice_3: 'Rhydon'
+},
+{
+    question: 'How does Pikachu evolve into Riachu?',
+    choice_1: 'Thunder stone',
+    choice_2: 'Lightning stone',
+    choice_3: 'Electric stone'
+},
+{
+    question: 'Why was Ashe late to picking his first Pokemon?',
+    choice_1: "His mother's car wouldnt start",
+    choice_2: 'He was ambushed by Team Rocket',
+    choice_3: 'He overslept'
+},
+{
+    question: 'What did Ashe steal from Misty?',
+    choice_1: 'A Gym Badge',
+    choice_2: 'A bike',
+    choice_3: 'A scooter'
+},
+{
+    question: 'How many Pokemon are there?',
+    choice_1: '172',
+    choice_2: '158',
+    choice_3: '151'
+},
+]
